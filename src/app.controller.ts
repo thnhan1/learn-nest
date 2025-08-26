b8d31102faac68d1a8d14509c26af34fb26991ae
Tran Huu Nhan <nhandev10@gmail.com>
2025-08-26 19:22:55 +0700

    refactor: update local config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Roles } from './decorator/roles.decorator';
-import { RolesGuard } from './guard/roles.guard';
 import { User } from './decorator/user.decorator';
+import { RolesGuard } from './guard/roles.guard';
 
 @Controller()
 export class AppController {
@@ -13,12 +12,10 @@ export class AppController {
         return this.appService.getHello();
     }
 
-
-    @Roles(['user'])
     @Get('admin')
     @UseGuards(RolesGuard)
     getAdminInfo() {
-        return this.appService.getAdminInfo()
+        return this.appService.getAdminInfo();
     }
 
     @Get('user')
